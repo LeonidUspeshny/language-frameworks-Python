448fda1e56d80eb59a99bebecab8cfdbed0bc98a
Leonid <graal@inbox.ru>
2024-07-11 16:43:53 +0300

    Initial commit to master branch

---
+++ News/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path
+
+
+from News.views import index, get_category
+urlpatterns = [
+    path('', index, name='Home'),
+    path('category/<int:category_id>/', get_category, name='Category')
+
+]
+
