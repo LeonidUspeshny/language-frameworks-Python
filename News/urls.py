f9a07a6b5c7f06e6428f7417e46513923d339aed
Leonid <graal@inbox.ru>
2024-07-23 19:29:51 +0300

    Исправлена ошибка в файле urls.py и добавлена рабочая ссылка(Rea more) в файел index.html

--- News/urls.py
+++ News/urls.py
@@ -7,7 +7,7 @@ from News.views import index, get_category, view_news
 urlpatterns = [
     path('', index, name='Home'),
     path('category/<int:category_id>/', get_category, name='Category'),
-    path('news/<int:news_id>/', view_news, name='View_news'),
+    path('news/<int:news_id>/', view_news, name='view_news'),
 
 ]
 
