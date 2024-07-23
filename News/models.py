8a105a850bc9fba5bd433b97889c9e49bb00e91e
Leonid <graal@inbox.ru>
2024-07-23 19:12:22 +0300

    ОБРАТНОЕ РАЗРЕШЕНИЕ АДРЕСОВ. СТАТИЧЕСКИЕ ФАЙЛЫ

--- News/models.py
+++ News/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.urls import reverse_lazy
+
+
 
 
 class News(models.Model):
@@ -10,13 +13,21 @@ class News(models.Model):
     is_published = models.BooleanField(default=True, verbose_name='Публикация')
     category = models.ForeignKey('Category', on_delete=models.PROTECT, null=True, verbose_name='Категория')
 
+
+    def get_absolute_url(self):
+        return reverse_lazy('view_news', kwargs={'news_id': self.pk})
+
     class Meta:
         verbose_name='Новости'
         verbose_name_plural='Новости'
         ordering = ['-created_at']
 
+
 class Category(models.Model):
     title = models.CharField(max_length=150, db_index=True, verbose_name='Категория')
+
+    def get_absolute_url(self):
+        return reverse_lazy('Category', kwargs={'category_id': self.pk})
 
     class Meta:
         verbose_name = 'Категория'
