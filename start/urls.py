2250300bd7c58b9c221a8edef09ef7dc75b2397e
Leonid <graal@inbox.ru>
2024-07-29 22:55:57 +0300

    Начало работы со стартовой страницей

--- start/urls.py
+++ start/urls.py
@@ -5,8 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.start, name='Start'),
     path('about', views.about),
-    path('lessons', views.lessons),
-    path('works', views.works),
-
 
 ]
--- start/views.py
+++ start/views.py
@@ -1,19 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 
 def start(request):
-    return HttpResponse("<h4>Проверка работы</h4>")
+    return render(request, 'start/start.html')
 
 
 def about(request):
-    return HttpResponse("<h4>Страница про нас</h4>")
-
-
-def lessons(request):
-    return HttpResponse("<h4>Мои занятия</h4>")
-
-
-def works(request):
-    return HttpResponse("<h4>Мои работы</h4>")
-# Create your views here.
+    return render(request, 'start/about.html')
