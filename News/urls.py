86a867cad795e2a6693f4bc13f753facd3f19041
Leonid <graal@inbox.ru>
2024-07-31 17:00:44 +0300

    Классы ListView, DetaiView, CreateView

--- News/urls.py
+++ News/urls.py
@@ -1,14 +1,18 @@
 from django.contrib import admin
 from django.urls import path
 
-
-from News.views import index, get_category, view_news, add_news
+from News.views import (HomeNews, NewsByCayegory, ViewNews, AddNews)
+# from News.views import index, get_category, view_news, add_news
 
 urlpatterns = [
-    path('', index, name='Home'),
-    path('category/<int:category_id>/', get_category, name='Category'),
-    path('news/<int:news_id>/', view_news, name='view_news'),
-    path('news/add_news', add_news, name='Add_news'),
+    # path('', index, name='Home'),
+    # path('category/<int:category_id>/', get_category, name='Category'),
+    # path('news/<int:news_id>/', view_news, name='view_news'),
+    # path('news/add_news', add_news, name='Add_news'),
+    path('', HomeNews.as_view(), name='Home'),
+    path('category/<int:category_id>/', NewsByCayegory.as_view(), name='Category'),
+    path('news/<int:pk>/', NewsByCayegory.as_view(), name='view_news'),
+    path('news/add_news', AddNews.as_view(), name='Add_news'),
 
 ]
 
