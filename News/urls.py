b6a84f5d28d34c519ad3f8f12e9aea4ec9a5dd14
Leonid <graal@inbox.ru>
2024-08-14 17:27:04 +0300

    Регистрация

--- News/urls.py
+++ News/urls.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 
-
-from News.views import (HomeNews, NewsByCategory, ViewNews, AddNews,)
+from News.views import HomeNews, NewsByCategory, ViewNews, AddNews, register, login
 # from News.views import index, get_category, view_news, add_news, test
 
 urlpatterns = [
@@ -10,11 +9,13 @@ urlpatterns = [
     # path('category/<int:category_id>/', get_category, name='Category'),
     # path('news/<int:news_id>/', view_news, name='view_news'),
     # path('news/add_news', add_news, name='Add_news'),
+    # path('test/', test, name='Test'),
     path('', HomeNews.as_view(), name='Home'),
     path('category/<int:category_id>/', NewsByCategory.as_view(), name='Category'),
     path('news/<int:pk>/', NewsByCategory.as_view(), name='View_news'),
     path('news/add_news', AddNews.as_view(), name='Add_news'),
-    # path('test/', test, name='Test'),
+    path('register', register, name='Register'),
+    path('login', login, name='Login'),
 
 ]
 
